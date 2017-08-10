e7733f8e1212528e41dc3a1316f52027325f2da4
Anton Kulyk <akulik@qualium-systems.com.ua>
2017-08-10 14:35:19 +0300

    Hide navbars

--- src/router.js
+++ src/router.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import {
-   StackNavigator,
-   addNavigationHelpers
-} from 'react-navigation';
+import { addNavigationHelpers, StackNavigator } from 'react-navigation';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import * as Screens from './screens';
 
-export const Navigator = StackNavigator({
-   Loading: { screen: Screens.LoadingScreen },
-   Login: { screen: Screens.LoginScreen },
-   Artists: { screen: Screens.ArtistsScreen }
+const navBarHidden = navigation => ({
+    header: null
 });
 
-const AppWithNavigationState = ({ dispatch, nav }) => <Navigator navigation={addNavigationHelpers({ dispatch, state: nav })}/>;
+export const Navigator = StackNavigator({
+    Loading: { screen: Screens.LoadingScreen, navigationOptions: navBarHidden },
+    Login: { screen: Screens.LoginScreen, navigationOptions: navBarHidden },
+    Artists: { screen: Screens.ArtistsScreen, navigationOptions: navBarHidden }
+});
+
+const AppWithNavigationState = ({ dispatch, nav }) => <Navigator
+    navigation={addNavigationHelpers({ dispatch, state: nav })}/>;
 
 AppWithNavigationState.propTypes = {
-   dispatch: PropTypes.func.isRequired,
-   nav: PropTypes.object.isRequired
+    dispatch: PropTypes.func.isRequired,
+    nav: PropTypes.object.isRequired
 };
 
 export default connect(state => ({ nav: state.nav }))(AppWithNavigationState);
